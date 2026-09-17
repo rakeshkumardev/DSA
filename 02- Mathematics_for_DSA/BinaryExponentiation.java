295d3b9e7dfc0bb04dbd49f2153fb01c7f747a6f
Rakesh_Kumar <rakesh.k091106@gmail.com>
2026-09-17 19:53:46 +0530

    added BinaryExponentiation Problem

---
+++ 02- Mathematics_for_DSA/BinaryExponentiation.java
@@ -0,0 +1,67 @@
+public class BinaryExponentiation {
+    public static void main(String[] args) {
+        System.out.println(BinaryExponentiation.myPow(2.0, 4));
+        System.out.println(BinaryExponentiation.myPowOptIter(2.0, 4));
+        System.out.println(BinaryExponentiation.myPowOptRec(2.0, 4));
+    }
+
+    // Brute-Force Approach
+    static double myPow(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            power = -power;
+            x = 1.0 / x;
+        }
+
+        double result = 1.0;
+        for (long i = 0; i < power; i++) {
+            result = result * x;
+        }
+        return result;
+    }
+
+    // Optimal Approach - iterative
+    static double myPowOptIter(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            power = -power;
+            x = 1.0 / x;
+        }
+
+        double result = 1.0;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result = result * x;
+                power = power - 1;
+            } else {
+                x = x * x;
+                power = power / 2;
+            }
+        }
+        return result;
+    }
+
+    // Optimal Approach - Recursive
+    static double myPowOptRec(double x, int n) {
+        long power = n;
+        if (power < 0) {
+            x = 1.0 / x;
+            power = -power;
+        }
+
+        return fastPowerHelper(x, n);
+    }
+
+    static double fastPowerHelper(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n % 2 == 0) {
+            return fastPowerHelper(x * x, n / 2);
+        } else {
+            return x * fastPowerHelper(x * x, n / 2);
+        }
+    }
+
+}
