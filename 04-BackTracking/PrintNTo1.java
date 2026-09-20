54a24231adb0e5d845f85df19db3c24a964a6291
Rakesh_Kumar <rakesh.k091106@gmail.com>
2026-09-20 16:05:11 +0530

    PrintNTo1 added

--- 04-BackTracking/PrintNTo1.java
+++ 04-BackTracking/PrintNTo1.java
@@ -1,10 +1,16 @@
 public class PrintNTo1 {
     public static void main(String[] args) {
-
+        PrintNTo1.printNTo1(1, 5);
     }
 
-    static void printNTo1(int n) {
+    // Print N to 1 using Backtracking (incrementing i, printing on return)
+    static void printNTo1(int i, int n) {
+        if (i > n) {
+            return;
+        }
 
+        printNTo1(i + 1, n);
+        System.out.println(i);
     }
 
 }
